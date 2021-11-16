44e7a848f876083bea80b0a54b9bde5641d3d2ae
Moritz Lang <hi@slashmo.codes>
2021-11-16 22:37:22 +0100

    Change async test sleep to 2 seconds

--- Tests/Tests/TestCase.swift
+++ Tests/Tests/TestCase.swift
@@ -6,6 +6,6 @@ final class TestCase: XCTestCase {
     }
 
     func test_async() async throws {
-        try await Task.sleep(nanoseconds: 1_000_000_000)
+        try await Task.sleep(nanoseconds: 2_000_000_000)
     }
 }
